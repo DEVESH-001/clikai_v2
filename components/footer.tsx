1f576e7ced7be2e602c2e76b35f7244e4c3bd628
DEVESH <dd4168@srmist.edu.in>
2025-02-20 16:54:53 +0530

    correction_5

--- components/footer.tsx
+++ components/footer.tsx
@@ -232,14 +232,14 @@ export function Footer() {
         {/* Copyright, legal links, and social media icons */}
         <div className="flex flex-col sm:flex-row justify-between items-center text-xs sm:text-sm text-gray-300">
           <div className="flex flex-wrap justify-center sm:justify-start space-x-2 sm:space-x-4 mb-2 sm:mb-0">
-            <Link href="/privacy-policy" className="hover:text-blue-300">
+            <Link href="/pages/privacy-policy" className="hover:text-blue-300">
               Privacy Policy
             </Link>
-            <Link href="/terms-and-conditions" className="hover:text-blue-300">
+            <Link href="/pages/terms-and-conditions" className="hover:text-blue-300">
               Terms & Conditions
             </Link>
             <Link
-              href="/about-us"
+              href="/pages/about-us"
               className="text-gray-300 hover:text-blue-300"
             >
               Contact Us
