d28e28f0645e96e1be8fe57031c30ca5373f33a7
DEVESH <dd4168@srmist.edu.in>
2025-02-22 19:09:15 +0530

    corrected services links

--- app/metadata.ts
+++ app/metadata.ts
@@ -22,27 +22,52 @@ export const descMetaData: Metadata = {
   description: "Clik.ai site disclaimer",
 };
 
-// export const termsAndConditionMetaData: Metadata = {
-//   title: "Disclaimer | Clik.ai",
-//   description:
-//     "Clik.ai site disclaimer",
-// };
-// export const termsAndConditionMetaData: Metadata = {
-//   title: "Terms and Conditions | Clik.ai",
-//   description:
-//     "Clik.ai - Supercharge your way to underwrite commercial real estate.",
-// };
-// export const termsAndConditionMetaData: Metadata = {
-//   title: "Terms and Conditions | Clik.ai",
-//   description:
-//     "Clik.ai - Supercharge your way to underwrite commercial real estate.",
-// };
-// export const termsAndConditionMetaData: Metadata = {
-//   title: "Terms and Conditions | Clik.ai",
-//   description:
-//     "Clik.ai - Supercharge your way to underwrite commercial real estate.",
-// };
+                                                      //PRODUCTS
+export const AutoUWMetaData: Metadata = {
+  title: "AutoUW – Automated Underwriting",
+  description:
+    "Streamline your underwriting process with AutoUW, our AI-powered tool designed for rapid, accurate underwriting in commercial real estate.",
+};
+export const InvestAssistMetaData: Metadata = {
+  title: "InvestAssist – Investment Assistance",
+  description:
+    " Empower your investment decisions with InvestAssist, offering data-driven insights and analysis to optimize your CRE investment strategies.",
+};
+export const SmartExtractMetaData: Metadata = {
+  title: "SmartExtract – Intelligent Data Extraction",
+  description:
+    " Optimize document processing with SmartExtract, our solution for fast, accurate extraction of critical data from commercial real estate files.",
+};
+export const Clarity360tMetaData: Metadata = {
+  title: "Loan Abstract – Document Data Abstraction",
+  description:
+    "Simplify loan document analysis with Loan Abstract, our tool designed to extract and abstract key data with precision from loan documents.",
+};
 
+                                                   //SERVICES
 
-
-
+export const UnderwritingDDSMetaData: Metadata = {
+  title: " Underwriting & Due Diligence Support",
+  description:
+    "Enhance your underwriting process with expert due diligence support services, ensuring comprehensive analysis and risk mitigation for your deals.",
+};
+export const CustomFDMMetaData: Metadata = {
+  title: " Custom Financial Models",
+  description:
+    "Enhance your underwriting process with expert due diligence support services, ensuring comprehensive analysis and risk mitigation for your deals.",
+};
+export const DisbursementPCNAMetaData: Metadata = {
+  title: " Disbursement & PCNA Support",
+  description:
+    "Streamline your financial operations with dedicated disbursement and PCNA support services, ensuring efficient fund management and processing.",
+};
+// export const LeaseAbstractionAuditMetaData: Metadata = {
+//   title: "  Lease Abstraction & Audit",
+//   description:
+//     "Streamline your financial operations with dedicated disbursement and PCNA support services, ensuring efficient fund management and processing.",
+// };
+// export const LeaseAbstractionAuditMetaData: Metadata = {
+//   title: "Loan Document Digitisation",
+//   description:
+//     "Streamline your financial operations with dedicated disbursement and PCNA support services, ensuring efficient fund management and processing.",
+// };
