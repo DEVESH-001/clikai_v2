d7f2dc420d01052a2bf58e6cdf8b73c6e5ef630b
DEVESH <dd4168@srmist.edu.in>
2025-03-06 19:33:35 +0530

    Added Bug Bounty

--- app/pages/services/components/DealFeasibilityChecker.tsx
+++ app/pages/services/components/DealFeasibilityChecker.tsx
@@ -525,7 +525,7 @@ const DealFeasibilityChecker = () => {
                 id="rentRollFile"
                 accept=".pdf, .xls, .xlsx"
                 onChange={handleRentRollChange}
-
+                required
                 className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
