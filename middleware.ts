90b9d1d8d2439bec3313a5d9e6806d9331f89379
hitesh_clikai <hitesh@clik.ai>
2025-03-26 14:36:46 +0530

    added 404

--- middleware.ts
+++ middleware.ts
@@ -10,20 +10,20 @@ export function middleware(request: NextRequest) {
   if (!path.startsWith("/api/")) {
 
     if (method !== "GET" && method !== "HEAD") {
-
+      // Create a new URL for the not-found page
       const notFoundUrl = new URL("/404", request.url)
       return NextResponse.redirect(notFoundUrl)
     }
   }
 
   // For all other requests, security headers
-  // const response = NextResponse.next()
-  // response.headers.set("X-XSS-Protection", "1; mode=block")
-  // response.headers.set("X-Frame-Options", "SAMEORIGIN")
-  // response.headers.set("X-Content-Type-Options", "nosniff")
-  // response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin")
+  const response = NextResponse.next()
+  response.headers.set("X-XSS-Protection", "1; mode=block")
+  response.headers.set("X-Frame-Options", "SAMEORIGIN")
+  response.headers.set("X-Content-Type-Options", "nosniff")
+  response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin")
 
-  //return response
+  return response
 }
 
 export const config = {
